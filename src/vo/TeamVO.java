4fb11a1407858a1ce64f7384a72d67175de60192
reallyLoveIT <1641711860@qq.com>
2015-03-09 21:32:18 +0800

    teamVO

--- src/vo/TeamVO.java
+++ src/vo/TeamVO.java
@@ -7,6 +7,298 @@ package vo;
  */
 public class TeamVO {
 	
+	/**
+	 * 球队全名
+	 */
+	public String name;
+	
+	/**
+	 * 球队名称缩写
+	 */
+	public String abbreviation;
+	
+	/**
+	 * 所在地
+	 */
+	public String location;
+	
+	/**
+	 * 赛区
+	 */
+	public String competion;
+	
+	/**
+	 * 分区
+	 */
+	public String partition;
+	
+	/**
+	 * 主场
+	 */
+	public String homeCourt;
+	
+	/**
+	 * 建立时间
+	 */
+	public String buildTime;
+	
+	/**
+	 * 比赛场数
+	 */
+	public String matchNum;
+	
+	/**
+	 * 投篮命中数
+	 */
+	public int totalScoreNum;
+	
+	/**
+	 * 投篮出手数
+	 */
+	public int totalShootNum;
+	
+	/**
+	 * 三分命中数
+	 */
+	public int totalThreePointScoreNum;
+	
+	/**
+	 * 三分出手数
+	 */
+	public int totalThreePointShootNum;
+	
+	/**
+	 * 罚球命中数
+	 */
+	public int totalFreeThrowScoreNum;
+	
+	/**
+	 * 罚球出手数
+	 */
+	public int totalFreeThrowShootNum;
+	
+	/**
+	 * 进攻篮板数
+	 */
+	public int totalOffensiveReboundsNum;
+
+	
+	/**
+	 * 防守篮板数
+	 */
+	public int totalDefensiveReboundsNum;
+	
+	/**
+	 * 总篮板数
+	 */
+	public int totalTotalReboundsNum;
+	
+	/**
+	 * 助攻数
+	 */
+	public int totalAssistNum;
+	
+	/**
+	 * 抢断数
+	 */
+	public int totalStealNum;
+	
+	/**
+	 * 盖帽数
+	 */
+	public int totalBlockNum;
+	
+	/**
+	 * 失误数
+	 */
+	public int totalTurnoverNum;
+	
+	/**
+	 * 犯规数
+	 */
+	public int totalFoulNum;
+	
+	/**
+	 * 比赛得分
+	 */
+	public int totalScore;
+	
+	/**
+	 * 投篮命中率
+	 */
+	public double totalScoreRate;
+	
+	/**
+	 * 三分命中率
+	 */
+	public double totalThreePointRate;
+	
+	/**
+	 * 罚球命中率
+	 */
+	public double totalFreeThrowRate;
+	
+	/**
+	 * 胜率
+	 */
+	public double totalWinningRate;
+	
+	/**
+	 * 进攻回合
+	 */
+	public double totalAttackRound;
+	
+	/**
+	 * 进攻效率
+	 */
+	public double totalAttackEfficiency;
+	
+	/**
+	 * 防守效率
+	 */
+	public double totalDefendEfficiency;
+	
+	/**
+	 * 篮板效率
+	 */
+	public double totalReboundEfficiency;
+	
+	/**
+	 * 抢断效率
+	 */
+	public double totalStealEfficiency;
+	
+	/**
+	 * 助攻率
+	 */
+	public double totalAssistRate;
 	
 	
+	
+	//以下为平均数据
+	/**
+	 * 评均投篮命中数
+	 */
+	public double aveScoreNum;
+	
+	/**
+	 * 评均投篮出手数
+	 */
+	public double aveShootNum;
+	
+	/**
+	 * 评均三分命中数
+	 */
+	public double aveThreePointScoreNum;
+	
+	/**
+	 * 评均三分出手数
+	 */
+	public double aveThreePointShootNum;
+	
+	/**
+	 * 评均罚球命中数
+	 */
+	public double aveFreeThrowScoreNum;
+	
+	/**
+	 * 评均罚球出手数
+	 */
+	public double aveFreeThrowShootNum;
+	
+	/**
+	 * 评均进攻篮板数
+	 */
+	public double aveOffensiveReboundsNum;
+
+	
+	/**
+	 * 评均防守篮板数
+	 */
+	public double aveDefensiveReboundsNum;
+	
+	/**
+	 * 评均总篮板数
+	 */
+	public double aveTotalReboundsNum;
+	
+	/**
+	 * 评均助攻数
+	 */
+	public double aveAssistNum;
+	
+	/**
+	 * 评均抢断数
+	 */
+	public double aveStealNum;
+	
+	/**
+	 * 评均盖帽数
+	 */
+	public double aveBlockNum;
+	
+	/**
+	 * 评均失误数
+	 */
+	public double aveTurnoverNum;
+	
+	/**
+	 * 评均犯规数
+	 */
+	public double aveFoulNum;
+	
+	/**
+	 * 评均比赛得分
+	 */
+	public double aveScore;
+	
+	/**
+	 * 评均投篮命中率
+	 */
+	public double aveScoreRate;
+	
+	/**
+	 * 评均三分命中率
+	 */
+	public double aveThreePointRate;
+	
+	/**
+	 * 评均罚球命中率
+	 */
+	public double aveFreeThrowRate;
+	
+	/**
+	 * 评均胜率
+	 */
+	public double aveWinningRate;
+	
+	/**
+	 * 评均进攻回合
+	 */
+	public double aveAttackRound;
+	
+	/**
+	 * 评均进攻效率
+	 */
+	public double aveAttackEfficiency;
+	
+	/**
+	 * 评均防守效率
+	 */
+	public double aveDefendEfficiency;
+	
+	/**
+	 * 评均篮板效率
+	 */
+	public double aveReboundEfficiency;
+	
+	/**
+	 * 评均抢断效率
+	 */
+	public double aveStealEfficiency;
+	
+	/**
+	 * 评均助攻率
+	 */
+	public double aveAssistRate;
 }
