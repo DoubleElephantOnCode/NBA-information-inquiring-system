34c6b3b4049a0240efd81d38841a373b52058c58
reallyLoveIT <1641711860@qq.com>
2015-03-14 16:09:28 +0800

    column  row

--- src/model/dataLogic/TeamList.java
+++ src/model/dataLogic/TeamList.java
@@ -49,4 +49,28 @@ public class TeamList {
 	public static ArrayList<TeamVO> getTeamVOList(){
 		return teamVOList;
 	}
+	
+	public static String[] getHeadListForColumn(){
+		String[] s= new String[]{
+				"总比赛场数","总投篮命中数","总投篮出手次数","总三分命中数",
+				"总三分出手数","总罚球命中数","总罚球出手数","总进攻篮板数","总防守篮板数","总篮板数",
+				"总助攻数","总抢断数","总盖帽数","总失误数","总犯规数",
+				"总比赛得分","投篮命中率","三分命中率",
+				"罚球命中率","胜率","进攻回合","进攻效率"
+				,"防守效率","进攻篮板效率","防守篮板效率","抢断效率","助攻率",   
+				"平均投篮命中数","平均投篮出手次数","平均三分命中数",
+				"平均三分出手数","平均罚球命中数","平均罚球出手数","平均进攻篮板数","平均防守篮板数","平均总篮板数",
+				"平均助攻数","平均抢断数","平均盖帽数","平均失误数","平均犯规数","平均比赛得分",
+		};
+		return s;
+	}
+
+	public static String[] getHeadListForRow(){
+		String[] s = new String[teamVOList.size()];
+		for(int i=0;i<teamVOList.size();i++){
+			s[i]=teamVOList.get(i).getAbbreviation();
+		}
+		return s;
+	}
+
 }
