13e34658e42ea96bdbcae5e8866abf22887f3fcf
HalaWKS <1028276446@qq.com>
2015-04-02 19:38:06 +0800

    测试的，记得导入jar包

---
+++ src/test/Console.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author HalaWKS
+ * @date 2015年4月2日 下午7:00:47
+ *
+ */
+public class Console {
+
+	/**
+	 * 
+	 * @param out
+	 * @param args
+	 */
+	public void execute(PrintStream out, String[] args){
+		
+	}
+	
+}
---
+++ src/test/TestMain.java
@@ -0,0 +1,22 @@
+package test;
+
+import test.data.PlayerHighInfo;
+
+/**
+ * 
+ * @author HalaWKS
+ * @date 2015年4月2日 下午7:00:51
+ *
+ */
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Console console = new Console();
+		console.execute(System.out, args);
+		
+		PlayerHighInfo p = new PlayerHighInfo();
+		
+	}
+
+}
