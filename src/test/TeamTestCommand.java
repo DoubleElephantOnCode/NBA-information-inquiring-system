eaff6aca3b9bb8d3281864c7bd563315a53b0ea0
reallyLoveIT <1641711860@qq.com>
2015-04-09 19:50:30 +0800

    teamtestcommand

--- src/test/TeamTestCommand.java
+++ src/test/TeamTestCommand.java
@@ -4,6 +4,11 @@ import test.data.TeamHighInfo;
 import test.data.TeamHotInfo;
 import test.data.TeamNormalInfo;
 
+/**
+ * 
+ * @author ZSK
+ * @date 2015年4月5日 下午4:03:22
+ */
 public class TeamTestCommand {
 	/**
 	 * Avg or Total
@@ -28,17 +33,25 @@ public class TeamTestCommand {
 	 */
 	boolean isNormal = true;
 	
-	
+	boolean isPositiveSequence = true;
 	
 	TeamHighInfo teamHighInfo = new TeamHighInfo();
 	TeamHotInfo teamHotInfo = new TeamHotInfo();
 	TeamNormalInfo teamNormalInfo = new TeamNormalInfo();
 	
 	public void analysisCommand(String[] args){
-		
+		for(int i = 0;i<args.length;){
+			i = analysisString(args,i);
+		}
 	}
 	
-	public void analysisString(String arg){
+	public int analysisString(String[] args,int i){
+		switch(args[i]){
+		case "-avg": i++; return i;
+		case "-total": i++; isAvg = false; return i;
+		case "-all": i++; return i;
 		
+		}
+		return 0;
 	}
 }
