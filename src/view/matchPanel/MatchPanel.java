fbf453d3d6a98557ca0c1507c0fd2d5f81c5e545
whj0401 <emls0401@163.com>
2015-04-26 19:08:50 +0800

    比赛显示

--- src/view/matchPanel/MatchPanel.java
+++ src/view/matchPanel/MatchPanel.java
@@ -2,8 +2,26 @@ package view.matchPanel;
 
 import javax.swing.JPanel;
 
-public class MatchPanel extends JPanel{
+import view.SizeAndLocationAndFont;
 
+public class MatchPanel extends JPanel{
 	
+	TeamStatisticsPanel t1, t2;
+
+	public MatchPanel(java.io.File team1, String[][] content1, String[] headListForRow1, String[] headListForColumn1,
+			java.io.File team2, String[][] content2, String[] headListForRow2, String[] headListForColumn2){
+		t1 = new TeamStatisticsPanel(team1, content1, headListForRow1, headListForColumn1);
+		t1.setLocation(0, 0);
+		
+		t2 = new TeamStatisticsPanel(team2, content2, headListForRow2, headListForColumn2);
+		t2.setLocation(0, t1.getHeight());
+		
+		this.add(t1);
+		this.add(t2);
+		this.setOpaque(false);
+		this.setLayout(null);
+		this.setSize(SizeAndLocationAndFont.matchPanelWidth, SizeAndLocationAndFont.matchPanelHeight);
+		this.setLocation(SizeAndLocationAndFont.matchPanelLocationX, SizeAndLocationAndFont.matchPanelLocationY);
+	}
 	
 }
---
+++ src/view/matchPanel/TestFrame.java
@@ -0,0 +1,94 @@
+package view.matchPanel;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JFrame;
+
+import view.SizeAndLocationAndFont;
+import view.infomationCenter.InformationCenterPanel;
+
+public class TestFrame {
+	static int row = 82, column = 10;
+	static int width = SizeAndLocationAndFont.frameWidth,
+			height = SizeAndLocationAndFont.frameHeight;
+	
+	static Point origin = new Point();
+	static JFrame f;
+	static String[][] content;
+	static String[] headListForRow;
+	static String[] headListForColumn;
+	static InformationCenterPanel back;
+	static MatchPanel p;
+	
+	String teamPath = "D:\\data\\teams\\MIA.svg";
+	
+	public TestFrame() {
+		// TODO Auto-generated constructor stub
+		content = new String[row][column];
+		headListForRow = new String[row];
+		headListForColumn = new String[column];
+		
+		back = new InformationCenterPanel(0);
+		
+		for(int i = 0; i < row; i++){
+			headListForRow[i] = i+"";
+			for(int j = 0; j < column; j++){
+				content[i][j] = i+" "+j;
+			}
+		}
+		
+		for(int i = 0; i < column; i++){
+			headListForColumn[i] = i+"";
+		}
+		
+		f = new JFrame();
+		f.setBackground(Color.red);
+		
+//		back.setSinglePlayerPanel("E:\\课件\\软件工程与计算\\大二下学期\\data\\迭代一数据\\players\\portrait\\LeBron James.png", "E:\\课件\\软件工程与计算\\大二下学期\\data\\迭代一数据\\players\\action\\LeBron James.png", null, content, headListForRow, headListForColumn);
+		java.io.File team = new java.io.File(teamPath);
+		p = new MatchPanel(team, content, headListForRow, headListForColumn, team, content, headListForRow, headListForColumn);
+		
+		back.add(p, 0);
+		
+		f.add(back);
+		f.setLayout(null);
+		f.setUndecorated(true);
+		f.setSize(width, height);
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+		
+		f.addMouseListener(
+		    	new MouseAdapter(){
+		    		public void mousePressed(MouseEvent e){
+		    			origin.x = e.getX();
+		                origin.y = e.getY();
+		            }
+		            public void mouseClicked(MouseEvent e) {
+		            	
+		            }
+		            public void mouseReleased(MouseEvent e) {
+		            	super.mouseReleased(e);
+		            }
+		            @Override
+		            public void mouseEntered(MouseEvent e) {
+		            	f.repaint();
+		            }
+		         });
+		f.addMouseMotionListener(
+	            new MouseMotionAdapter(){
+		              public void mouseDragged(MouseEvent e){
+		                Point p =    f.getLocation();
+		                f.setLocation(p.x + e.getX() - origin.x, p.y + e.getY() - origin.y );
+		              }
+		            }
+		        );
+	}
+	
+	public static void main(String[] args){
+		new TestFrame();
+	}
+}
