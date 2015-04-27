7328febd81864165dc90ae6524cd4cfa0e58d99d
reallyLoveIT <1641711860@qq.com>
2015-04-27 21:32:38 +0800

    again

--- src/control/ShowTeamController.java
+++ src/control/ShowTeamController.java
@@ -2,15 +2,18 @@ package control;
 
 import java.util.Date;
 
-import model.dataLogic.SingleTeamModel;
+import model.dataLogic.ShowHotTeamModel;
 import model.dataLogic.ShowTeamDataModel;
+import model.dataLogic.SingleTeamModel;
 
 public class ShowTeamController {
 	ShowTeamDataModel showTeamModel;
 	SingleTeamModel singleTeamModel;
+	ShowHotTeamModel showHotTeamModel;
 	public ShowTeamController(){
 		showTeamModel = new ShowTeamDataModel();
 		singleTeamModel = new SingleTeamModel();
+		showHotTeamModel = new ShowHotTeamModel();
 	}
 	
 	public void showTeamTable(){
@@ -26,6 +29,6 @@ public class ShowTeamController {
 	}
 	
 	public void showHotTeamTable(int i){
-		
+		showHotTeamModel.showTeamTable(i);
 	}
 }
