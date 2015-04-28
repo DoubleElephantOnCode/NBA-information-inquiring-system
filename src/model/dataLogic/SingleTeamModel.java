ba9b5cffad85a47f79baecd19def3b439b3281b6
reallyLoveIT <1641711860@qq.com>
2015-04-28 19:37:05 +0800

    动态做完

--- src/model/dataLogic/SingleTeamModel.java
+++ src/model/dataLogic/SingleTeamModel.java
@@ -3,11 +3,48 @@ package model.dataLogic;
 import java.io.File;
 import java.util.Date;
 
+import model.readData.ReadMatchData;
 import view.mainFrame.Main;
 import vo.TeamVO;
 
-public class SingleTeamModel {
+public class SingleTeamModel implements ShowView{
+	public String teamName;
+	public Date begin;
+	public Date end;
+	
 	public void showTeam(String teamName, Date begin,Date end){
+		
+		this.teamName = teamName;
+		this.begin = begin;
+		this.end = end;
+		
+		ReadMatchData.readMatch.setCurrentView(this);
+		
+		TeamVO teamVO = TeamList.findTeamVO(teamName);
+		File svgFile = new File(teamVO.getPath());
+		String[] infoName = TeamList.getTeamInfoName();
+		String[] info = teamVO.toStringArrayForSingleTeam();
+		
+		String[] headListForColumn = MatchList.getHeadListForColumn();
+		String[][] content = teamVO.getMatchInfo(begin,end);
+		String[][] realContent = new String[content.length][3];//去掉第一项时间
+		for(int i = 0 ;i<content.length;i++){
+			for(int j = 0;j<3;j++){
+				realContent[i][j] = content[i][j+1];
+			}
+		}
+		
+		
+		String[] headListForRow = new String[content.length];
+		for(int i = 0;i<content.length;i++){
+			headListForRow[i] = content[i][0];
+		}
+		Main.setSingleTeamPanel(svgFile, infoName, info, realContent, headListForRow, headListForColumn,teamName);
+	}
+
+	@Override
+	public void changeData() {
+		// TODO Auto-generated method stub
 		TeamVO teamVO = TeamList.findTeamVO(teamName);
 		File svgFile = new File(teamVO.getPath());
 		String[] infoName = TeamList.getTeamInfoName();
