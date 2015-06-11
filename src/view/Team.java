d14d942a3a294a93784e23ac4760f35363159401
whj0401 <emls0401@163.com>
2015-06-11 19:54:48 +0800

    球员对比界面

---
+++ src/view/Team.java
@@ -0,0 +1,15 @@
+package view;
+
+public class Team {
+
+	public final static String[] TEAM = {"老鹰", "篮网", "凯尔特人", "黄蜂", "公牛", "骑士", "小牛",
+										"掘金", "活塞", "勇士", "火箭", "步行者", "快船", "湖人",
+										"灰熊", "热火", "雄鹿", "森林狼", "鹈鹕", "尼克斯", "雷霆",
+										"魔术", "76人", "太阳", "开拓者", "国王", "马刺", "猛龙", "爵士", "奇才"};
+	
+	public final static String[] TEAMEN = {"ATL", "BKN", "BOS", "CHA", "CHI", "CLE", "DAL", 
+											"DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL",
+											"MEM", "MIA", "MIL", "MIN", "NOP", "NYK", "OKC",
+											"ORL", "PHI", "PHX", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"};
+	
+}
--- src/view/playerCompare/PlayerComparePanel.java
+++ src/view/playerCompare/PlayerComparePanel.java
@@ -1,8 +1,13 @@
 package view.playerCompare;
 
 import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import org.jfree.chart.ChartPanel;
@@ -13,13 +18,15 @@ import statisticsAnalysis.RadarChart;
 import statisticsAnalysis.TimeSeriesChart;
 import statisticsAnalysis.playerCompare.SingleAbilityCmp;
 import view.SizeAndLocationAndFont;
+import view.Team;
 import view.selectedPanel.SelectPanel;
 
 public class PlayerComparePanel extends JPanel{
 	static String chart;
 	static int type;
 	
-	boolean p1 = false, p2 = false;
+	static boolean p1 = false, p2 = false;
+	static String PLAYER1, PLAYER2;
 	
 	static SingleAbilityCmp sac;
 	static TimeSeriesChart tsc;
@@ -31,11 +38,14 @@ public class PlayerComparePanel extends JPanel{
 	static SelectPanel season, after, chartType, chartContent;
 	static SelectPanel team1, player1, team2, player2;
 	
+	static JLabel player_1, player_2;
+	
 	static String[] SEASON = {"11-12", "12-13", "13-14", "14-15"},
 			AFTER = {"常规赛", "季后赛"},
 			CHARTTYPE = {"稳定性", "发挥状态"},
 			CHARTCONTENT = {"得分", "篮板", "助攻", "抢断", "盖帽", "失误", "犯规"},
-			TEAM = {""};
+			TEAM = Team.TEAM,
+			TEANEN = Team.TEAMEN;
 	
 	
 	public PlayerComparePanel(){
@@ -46,45 +56,68 @@ public class PlayerComparePanel extends JPanel{
 	
 	public PlayerComparePanel(String player_1Pic, double[] ability_1, String player_1Name, TimeSeries series1,
 			String player_2Pic, double[] ability_2, String player_2Name, TimeSeries series2){
+		setTimeSeriesPanel(series1, series2);
+		setRadarChart(ability_1, player_1Name, ability_2, player_2Name);
+	}
+	
+	public PlayerComparePanel(String player_1Pic, double[] ability_1, String player_1Name, ArrayList<Double> list1,
+			String player_2Pic, double[] ability_2, String player_2Name, ArrayList<Double> list2){
+		setBoxPanel(player_1Name, list1, player_2Name, list2);
+		setRadarChart(ability_1, player_1Name, ability_2, player_2Name);
+	}
+	
+	public void setPlayerPic(String playerPic, int index){
+		if(index == 1){
+			if(player_1 != null) this.remove(player_1);
+			player_1 = setJLabelWithIcon(playerPic, SizeAndLocationAndFont.playerComparePanelPhotoWidth, SizeAndLocationAndFont.playerComparePanelPhotoHeight);
+			player_1.setLocation(SizeAndLocationAndFont.playerComparePanelPhoto_1LocationX, SizeAndLocationAndFont.playerComparePanelPhoto_1LocationY);
+			this.add(player_1);
+		}
+		else if(index == 2){
+			if(player_2 != null) this.remove(player_2);
+			player_2 = setJLabelWithIcon(playerPic, SizeAndLocationAndFont.playerComparePanelPhotoWidth, SizeAndLocationAndFont.playerComparePanelPhotoHeight);
+			player_2.setLocation(SizeAndLocationAndFont.playerComparePanelPhoto_2LocationX, SizeAndLocationAndFont.playerComparePanelPhoto_2LocationY);
+			this.add(player_2);
+		}
+	}
+	
+	public void setRadarChart(double[] ability_1, String player_1Name, double[] ability_2, String player_2Name){
+		if(radar != null) this.remove(radar);
+		XYSeries s1 = new XYSeries(player_1Name), s2 = new XYSeries(player_2Name);
+		rc = new RadarChart(null, ability_1.length);
+		for(int i = 0; i < ability_1.length; i++){
+			s1.add(i*rc.angle, ability_1[i]);
+			s2.add(i*rc.angle, ability_2[i]);
+		}
+		rc.add(s1);
+		rc.add(s2);
+		radar = rc.getChartPanel(Color.LIGHT_GRAY, true);
+		radar.setSize(SizeAndLocationAndFont.playerComparePanelRadarChartWidth, SizeAndLocationAndFont.playerComparePanelRadarChartHeight);
+		radar.setLocation(SizeAndLocationAndFont.playerComparePanelRadarChartLocationX, SizeAndLocationAndFont.playerComparePanelRadarChartLocationY);
+		this.add(radar);
+	}
+	
+	public void setTimeSeriesPanel(TimeSeries series1, TimeSeries series2){
+		if(p != null)
+			this.remove(p);
 		tsc = new TimeSeriesChart(null, "时间", chart);
 		tsc.add(series1);
 		tsc.add(series2);
 		p = tsc.getChartPanel();
 		p.setSize(SizeAndLocationAndFont.playerComparePanelChart_2Width, SizeAndLocationAndFont.playerComparePanelChart_2Height);
 		p.setLocation(SizeAndLocationAndFont.playerComparePanelChart_2LocationX, SizeAndLocationAndFont.playerComparePanelChart_2LocationY);
-		
-		XYSeries s1 = new XYSeries(player_1Name), s2 = new XYSeries(player_2Name);
-		rc = new RadarChart(null, ability_1.length);
-		for(int i = 0; i < ability_1.length; i++){
-			s1.add(i*rc.angle, ability_1[i]);
-			s2.add(i*rc.angle, ability_2[i]);
-		}
-		rc.add(s1);
-		rc.add(s2);
-		radar = rc.getChartPanel(Color.LIGHT_GRAY, true);
-		radar.setSize(SizeAndLocationAndFont.playerComparePanelRadarChartWidth, SizeAndLocationAndFont.playerComparePanelRadarChartHeight);
-		radar.setLocation(SizeAndLocationAndFont.playerComparePanelRadarChartLocationX, SizeAndLocationAndFont.playerComparePanelRadarChartLocationY);
+		this.add(p);
 	}
 	
-	public PlayerComparePanel(String player_1Pic, double[] ability_1, String player_1Name, ArrayList<Double> list1,
-			String player_2Pic, double[] ability_2, String player_2Name, ArrayList<Double> list2){
+	public void setBoxPanel(String player_1Name, ArrayList<Double> list1, String player_2Name, ArrayList<Double> list2){
+		if(p != null)
+			this.remove(p);
 		sac = new SingleAbilityCmp(chart, player_1Name, list1, player_2Name, list2);
 		p = sac.getChartPanel();
 		
 		p.setSize(SizeAndLocationAndFont.playerComparePanelChart_2Width, SizeAndLocationAndFont.playerComparePanelChart_2Height);
 		p.setLocation(SizeAndLocationAndFont.playerComparePanelChart_2LocationX, SizeAndLocationAndFont.playerComparePanelChart_2LocationY);
-		
-		XYSeries s1 = new XYSeries(player_1Name), s2 = new XYSeries(player_2Name);
-		rc = new RadarChart(null, ability_1.length);
-		for(int i = 0; i < ability_1.length; i++){
-			s1.add(i*rc.angle, ability_1[i]);
-			s2.add(i*rc.angle, ability_2[i]);
-		}
-		rc.add(s1);
-		rc.add(s2);
-		radar = rc.getChartPanel(Color.LIGHT_GRAY, true);
-		radar.setSize(SizeAndLocationAndFont.playerComparePanelRadarChartWidth, SizeAndLocationAndFont.playerComparePanelRadarChartHeight);
-		radar.setLocation(SizeAndLocationAndFont.playerComparePanelRadarChartLocationX, SizeAndLocationAndFont.playerComparePanelRadarChartLocationY);
+		this.add(p);
 	}
 	
 	private void inititalSelectPanel(){
@@ -119,6 +152,26 @@ public class PlayerComparePanel extends JPanel{
 		player2 = new SelectPanel(SizeAndLocationAndFont.playerComparePanelSelectPlayer_2_NameWidth, SizeAndLocationAndFont.playerComparePanelSelectPlayer_2_NameHeight);
 		player2.setLocation(SizeAndLocationAndFont.playerComparePanelSelectPlayer_2_NameLocationX, SizeAndLocationAndFont.playerComparePanelSelectPlayer_2_NameLocationY);
 	
+		team1.box.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				team1.action();
+				int s = team1.getSelectedIndex();
+				
+			}
+		});
+		
+		team2.box.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				team2.action();
+				int s = team2.getSelectedIndex();
+				
+			}
+		});
+		
 		this.add(team1);
 		this.add(team2);
 		this.add(player1);
@@ -130,6 +183,15 @@ public class PlayerComparePanel extends JPanel{
 		this.setSize(SizeAndLocationAndFont.playerComparePanelWidth, SizeAndLocationAndFont.playerComparePanelHeight);
 		this.setLayout(null);
 		this.setLocation(SizeAndLocationAndFont.playerComparePanelLocationX, SizeAndLocationAndFont.playerComparePanelLocationY);
+	}
+	
+	private static JLabel setJLabelWithIcon(String IconPath, int width, int height){
+		ImageIcon icon = new ImageIcon(IconPath);
+		icon.setImage(icon.getImage().getScaledInstance(width, height,Image.SCALE_DEFAULT));
+		JLabel label = new JLabel(icon);
+		label.setSize(width, height);
+		label.setOpaque(false);
+		return label;
 	}
 	
 }
